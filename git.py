e89b729709c6e417fd5d733df131a37d5044f257
AmanAgarwal041 <agarwal.aman041@gmail.com>
2016-01-06 00:48:44 +0530

    Added new functionalities to the git Controls

--- git.py
+++ git.py
@@ -19,9 +19,23 @@ class Git(object) :
         self.path = path if(path != "") else os.getcwd()
         self.functionalities = {
                                     0:"Select any one and pass the parameters separated by space",
-                                    1:"Input 1 and a project name to create a directory (optional) for initialising git repo."
+                                    1:"Input 1 and a project name to create a directory (optional) for initialising git repo.",
+                                    2:"Input 2 to pull the data from the git repo.",
+                                    3:"Input 3 to push the data to the git repo.",
+                                    4:"Input 4 to commit the changes to the git repo.",
+                                    5:"Input 5 to add the remote origin to the git repo.",
+                                    6:"Input 6 to add the new created files to the stack for committing the changes",
+                                    7:"Input 7 to change the directory"
                                 }
-        self.functionValues = {1:self.initialisation}
+        self.functionValues = {
+                                1:self.initialisation,
+                                2:self.pull_changes,
+                                3:self.push_changes,
+                                4:self.commit_changes,
+                                5:self.remote_origin,
+                                6:self.add_files,
+                                7:self.change_directory
+                                }
 
 
 
@@ -39,21 +53,101 @@ class Git(object) :
             print "*******************\n"
             input = raw_input().strip().split(" ")
             if((input[0].isdigit()) and (int(input[0]) >= 1  and int(input[0]) <= len(self.functionValues))):
-                print self.functionValues[int(input[0])](input[1] if(len(input)==2) else str(uuid.uuid1()))
+                print self.functionValues[int(input[0])](" ".join(input[1:len(input)]) if(len(input)>1) else "")
             else:
                 break
 
 
-    def initialisation(self,directory):
+    def initialisation(self,inputValues):
         '''
             Initialisation of git project folder.
         '''
+        if(inputValues==""):
+            inputValues = [str(uuid.uuid1())]
+        else:
+            inputValues = inputValues.split(" ")
+
         try:
-            os.mkdir(self.path+directory)
-            os.chdir(self.path+directory)
+            os.mkdir(self.path+inputValues[0])
+            os.chdir(self.path+inputValues[0])
             return subprocess.check_output("git init" , shell=True)
         except OSError:
             return "failed"
 
-git = Git("/media/aman/E:/Test-git-script/")
+
+    def pull_changes(self,inputValues):
+        '''
+            Pull the data of the git project folder.
+        '''
+        try:
+            if(inputValues!=""):
+                inputValues = inputValues.split(" ")
+                os.chdir(self.path+inputValues[0])
+            return subprocess.check_output("git pull -v --progress" , shell=True)
+        except OSError:
+            return "failed"
+
+    def push_changes(self,inputValues):
+        '''
+            Push the data to the git project folder.
+        '''
+        try:
+            if(inputValues!=""):
+                inputValues = inputValues.split(" ")
+                os.chdir(self.path+inputValues[0])
+            return subprocess.check_output("git push -v --progress origin master" , shell=True)
+        except OSError:
+            return "failed"
+
+    def commit_changes(self,inputValues):
+        '''
+            Commit the changes to the git project folder.
+        '''
+        try:
+            if(inputValues==""):
+                '''
+                    Default Message added to the Commit changes
+                '''
+                inputValues = "Added changes to the repository"
+            return subprocess.check_output("git commit -am \""+inputValues+"\"" , shell=True)
+        except OSError:
+            return "failed"
+
+    def remote_origin(self,inputValues):
+        '''
+            Remote Adding Origin repository link to the .git configuration file
+        '''
+        try:
+            if(inputValues!=""):
+                return subprocess.check_output("git remote add origin "+inputValues , shell=True)
+            else:
+                return "Changes cannot be done as the url is not provided"
+        except OSError:
+            return "failed"
+
+
+    def add_files(self,inputValues):
+        '''
+            Adding files to the stack for commiting the changes
+        '''
+        try:
+            if(inputValues==""):
+                return subprocess.check_output("git add *" , shell=True)
+        except OSError:
+            return "failed"
+
+
+    def change_directory(self,inputValues):
+        '''
+            Changing directory to follow git commands
+        '''
+        try:
+            if(inputValues!=""):
+                os.chdir(self.path+inputValues)
+            else:
+                return "Enter the directory"
+        except OSError:
+            return "failed"
+
+git = Git("/media/aman/E:/GIT/")
 git.following_process()
